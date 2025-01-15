da0eefefa5794d4d5da0d5fd70a2d1af459d04a4
Kamilya <kamilya@Kamilyas-MacBook-Pro.local>
2025-01-16 01:05:04 +1100

    Initial project setup: Added Webpack configuration for bundling and managing project assets.

---
+++ webpack.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import webpack from 'webpack';
+import { BuildPaths } from './config/build/types/config';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+
+const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+};
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev,
+});
+
+export default config;
